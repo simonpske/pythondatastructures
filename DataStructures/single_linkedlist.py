f5cda7df2fddc462572970b3eef388e76574e24a
keller.p <philipp.keller@creos.net>
2025-03-03 13:50:00 +0100

    Creation of single and double linked list

---
+++ DataStructures/single_linkedlist.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+
+        # Move to the tail (the last node)
+        node = self.head
+        while node.next:
+            node = node.next
+
+        # Here we have moved to the end and we add here the value of the last node
+        node.next = Node(value)
+        return
+
+    def to_list(self):
+        out_list = []
+
+        node = self.head
+        while node:
+            out_list.append(node.value)
+            node = node.next
+
+        return out_list
+
+linked_list = LinkedList()
+linked_list.append(3)
+linked_list.append(2)
+linked_list.append(-1)
+linked_list.append(0.2)
+
+print ("Pass" if  (linked_list.to_list() == [3, 2, -1, 0.2]) else "Fail")
